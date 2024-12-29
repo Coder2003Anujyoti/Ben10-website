3498ba8fbb0b710eab7f5fb36329092e8f04bd8d
Coder2003Anujyoti <anujyotide@gmail.com>
2024-12-29 10:39:01 +0530

    Update

--- App.js
+++ App.js
@@ -11,10 +11,33 @@ app.get('/',
             Hello Gfg!</h1>`
         );
     });
-app.get('/aliens', (req, res) => {
+app.get('/api/aliens', (req, res) => {
+  const limit = parseInt(req.query.limit)||aliens.length;  // Default to 10 if not provided
+  const offset = parseInt(req.query.offset)||0;  // Default to 0 if not provided
+if(isNaN(limit) && limit<=0){
+  return res.status(400).json({error:"Limit must be a positive number."})
+}
+if(isNaN(offset) && offset<0){
+  return res.status(400).json({error:"Offset must be a non-negative number."})
+}
+
 // Send the posts array as a JSON response
-return res.json(aliens);
+return res.json(aliens.slice(offset,offset+limit));
 });
+app.get('/aliens',(req,res)=>{
+  const html=`
+  <ul>
+  ${aliens.map(i=>`<li>${i.name}</li>`).join("")}
+  </ul>
+  `
+  res.send(html);
+})
+app.get('/api/aliens/:id',(req,res)=>{
+  const id=Number(req.params.id);
+  const alien=aliens.find(i=> i.id===id)
+  return res.json(alien);
+})
+
 app.listen(PORT,
     () => {
         console.log(
