67878cbcfa74dab2bb6caf746430c13017ca154f
Coder2003Anujyoti <anujyotide@gmail.com>
2025-01-30 13:42:31 +0530

    Changes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs=require('fs');
 const app = express();
-const PORT = 8001;
+const PORT = 8000;
 const aliens=require("./aliens.json");
 const cors=require('cors');
 const bodyParser=require('body-parser');
@@ -12,60 +12,40 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 // define the route
-app.get('/',
-    (req, res) => {
-        res.send(
-            `<h1 style="color: green;">
-            Hello Gfg!</h1>`
-        );
+app.get('/',(req, res) => {
+      return res.json(aliens);
     });
-app.get('/api/aliens', (req, res) => {
-  const limit = parseInt(req.query.limit)||aliens.length;  // Default to 10 if not provided
-  const offset = parseInt(req.query.offset)||0;  // Default to 0 if not provided
+app.get('/aliens', (req, res) => {
+  const limit = parseInt(req.query.limit)||aliens.length; 
+  const offset = parseInt(req.query.offset)||0;
 if(isNaN(limit) && limit<=0){
   return res.status(400).json({error:"Limit must be a positive number."})
 }
 if(isNaN(offset) && offset<0){
   return res.status(400).json({error:"Offset must be a non-negative number."})
 }
-
-// Send the posts array as a JSON response
-return res.json(aliens.slice(offset,offset+limit));
+return res.json({
+  data:aliens.slice(offset,offset+limit),
+  length:aliens.length
+  });
 });
-app.get('/html/aliens',(req,res)=>{
-  const html=`
-  <ul>
-  ${aliens.map(i=>`<li>${i.name}</li>`).join("")}
-  </ul>
-  `
-  res.send(html);
-})
-app.get('/api/aliens/:id',(req,res)=>{
-  const id=Number(req.params.id);
-  const alien=aliens.find(i=> i.id===id)
-  return res.json(alien);
-})
-app.post('/api/aliens',(req,res)=>{
-  const data={
-    id:req.body.id,
-    name:req.body.name,
-    image:req.body.image
-  }
-  aliens.push(data);
-  fs.writeFile("./aliens.json",JSON.stringify(aliens));
-  return res.json(aliens);
-})
-app.get('/aliens',async(req,res)=>{
+app.get('/api',async(req,res)=>{
   const text=req.query.name;
-  if(text!=undefined){
-  const filteredItems = aliens.filter(item => item.name.toLowerCase().includes(text.toLowerCase()));
-      // Check if all letters in the item name exist in the provided text
-    // Return the filtered items
-    res.json(filteredItems);
-    }
-    else{
-      res.json(aliens);
-    }
+  const limit = parseInt(req.query.limit)||aliens.length; 
+  const offset = parseInt(req.query.offset)||0;
+if(isNaN(limit) && limit<=0){
+  return res.status(400).json({error:"Limit must be a positive number."})
+}
+if(isNaN(offset) && offset<0){
+  return res.status(400).json({error:"Offset must be a non-negative number."})
+}
+  const filteredItems = aliens.filter((item) => item.name.toLowerCase().includes(text.trim().toLowerCase()));
+      
+   return res.json({
+     data:filteredItems.slice(offset,offset+limit),
+     length:filteredItems.length
+   });
+  
 })
 
 app.listen(PORT,
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,76 +1,108 @@
 import React,{useState,useEffect} from "react";
 function Navbar(){
   const [count,setCount]=useState(0);
+  const [length,setLength]=useState(-1);
+  const [subcount,setSubcount]=useState(0);
   const [datas,setDatas]=useState([]);
    const [text,setText]=useState("");
    const [items,setItems]=useState([]);
    const [loads,setLoads]=useState(false);
    const [offset,setOffset]=useState(0);
-   const [load,setLoad]=useState(false);
-   const api=async()=>{
-     const res=await fetch(`https://ben10-website.onrender.com/aliens?name=${text}`);
-     const data =await res.json();
-     setDatas(data);
-     setLoad(true);
-     setCount(count+1);
-   }
+   const [load,setLoad]=useState(true);
+   const [suboffset,setSuboffset]=useState(0);
+   const [toggle,setToggle]=useState(false);
    const getaliens=async()=>{
-     const res=await fetch(`https://ben10-website.onrender.com/api/aliens?offset=${offset}&limit=5`);
+     const res=await fetch(`http://localhost:8000/aliens?offset=${offset}&limit=5`);
      const data =await res.json();
-     setItems([...items,data]);
-     setLoad(true);
+     setItems([...items,data.data]);
+     setCount(data.length)
+     setLoad(false);
      setLoads(false);
    }
-   useEffect(()=>{
-     if(text!=''){
-     api();
-     }
-   },[text])
-   useEffect(()=>{
-     if(text=='' && offset<66){
-       getaliens()
-     }
-   },[offset])
+   const getname=async()=>{
+     const res=await fetch(`http://localhost:8000/api?name=${text}&offset=${suboffset}&limit=5`);
+     const data =await res.json();
+     setLength(data.data.length)
+     setDatas([...datas,data.data]);
+     setSubcount(data.length)
+     setLoad(false);
+     setLoads(false);
+     setToggle(false)
+    
+   }
    const press=()=>{
      setLoads(true);
      setOffset(offset+5)
    }
+   const subpress=()=>{
+     setToggle(true)
+     setLoads(true);
+     setSuboffset(suboffset+5)
+   }
+   const go=()=>{
+     if(text.trim()===''){
+       alert("Please Input text!!!");
+     }
+     else{
+       setToggle(true)
+       setDatas([])
+       setLoad(true);
+       setSuboffset(0);
+     }
+   }
+   useEffect(()=>{
+     if(toggle===true)
+     getname()
+   },[toggle])
+   useEffect(()=>{
+       getaliens()
+   },[offset])
   return(<>
-    <div className="w-full h-16 bg-green-600 flex items-center gap-x-6 lg:gap-x-64">
+    <div className="w-full h-16 bg-green-600 flex items-center gap-x-6 md:gap-x-64">
       <div>
         <img src="images/Ben.png" className="w-16 h-16 m-2"/>
       </div>
-      <div>
-        <input type="text" value={text} onChange={(e)=>{
-        setLoad(false)
-        setText(e.target.value)}} placeholder="Search for aliens..." className="m-4 md:w-96 focus:outline-none bg-black rounded-md text-slate-100" />
       </div>
-    </div>
+<div className="w-full flex flex-row justify-center py-4">
+  <input type="text" value={text} onChange={(e)=>{setText(e.target.value)
+     setDatas([])
+     setLength(-1)
+  }} placeholder="Search for aliens..."  className="bg-black rounded-l-lg text-slate-100 focus:outline-none" />
+<button className="text-white font-extrabold text-sm p-2 rounded-r-lg bg-slate-800" onClick={go}>Submit</button>
+      </div>
     <div className="my-2">
-    {load==false && text!='' && <>
+    {load==true && <>
       <div className=" w-full flex  justify-center align-center my-48  md:my-32">
       <h1 className="font-bold " >Page is Loading...</h1>
       </div>
-    </>}  
-        {load==false && count==0 && text=='' && <>
-      <div className=" w-full flex  justify-center align-center my-48  md:my-32">
-      <h1 className="font-bold " >Page is Loading...</h1>
-      </div>
-    </>}  
+    </>} 
+    {
+      load==false && <>
     <div className=" flex justify-around flex-wrap gap-y-4 md:flex md:flex-row md:flex-wrap  md:justify-start md:mx-16 md:gap-x-2">
-      {text!='' && load==true && datas.map((alien) => {
-       return(<>
-        <div className="bg-white rounded-lg text-center p-1 border shadow-lg shadow-green-500/30 hover:scale-105 font-bold hover:ease-in-out duration-300 md:p-4 md:max-h-fit"  key={alien.id}>
+       {text.trim()!='' &&
+        datas.map((alien,index) => {
+         return(<>{alien.map((i)=>{return(<>
+                   <div className="bg-white rounded-lg text-center p-1 border shadow-lg shadow-green-500/30 hover:scale-105 font-bold hover:ease-in-out duration-300 md:p-4 md:max-h-fit"  key={i.id}>
           <img className="h-40 w-32"
-            src={Array.from(alien.image).reverse().slice(33).reverse().join("")}
-            alt={alien.name}
+            src={Array.from(i.image).reverse().slice(33).reverse().join("")}
+            alt={i.name}
          
           />
-          <p className="my-2">{alien.name}</p>
+          <p className="my-2">{i.name}</p>
         </div>
-        </>)
+           
+           
+         </>)})}
+         {index==datas.length-1 && 
+         <div className="w-full flex justify-center">
+      {suboffset<subcount-5 && loads==false && <img src="images/Blogo.png" className="w-8 h-8 md:w-12 md:h-12" onClick={subpress}></img>}
+    {loads==true && <div className="flex items-center font-bold"><p>Items are loading...</p></div> }
+         </div>
+         }
+         </>)
+
       })}
-       {text=='' && items.map((alien,index) => {
+       {text.trim()=='' && items.map((alien,index) => {
          return(<>{alien.map((i)=>{return(<>
                    <div className="bg-white rounded-lg text-center p-1 border shadow-lg shadow-green-500/30 hover:scale-105 font-bold hover:ease-in-out duration-300 md:p-4 md:max-h-fit"  key={i.id}>
           <img className="h-40 w-32"
@@ -85,7 +117,7 @@ function Navbar(){
          </>)})}
          {index==items.length-1 && 
          <div className="w-full flex justify-center">
-      {offset<65 && loads==false && <img src="images/Blogo.png" className="w-8 h-8 md:w-12 md:h-12" onClick={press}></img>}
+      {offset<count-5 && loads==false && <img src="images/Blogo.png" className="w-8 h-8 md:w-12 md:h-12" onClick={press}></img>}
     {loads==true && <div className="flex items-center font-bold"><p>Items are loading...</p></div> }
          </div>
          }
@@ -93,7 +125,8 @@ function Navbar(){
 
       })}
     </div>
-   {datas.length==0 && text!="" && load==true &&
+    </>}
+   {length===0  && load==false  &&
    <>
      <div className=" w-full flex  justify-center align-center my-48  md:my-32">
    <h1 className=" font-bold">No Aliens</h1>
