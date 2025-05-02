dfce3d7b6b4b453b4d970604b5e3f135aafb7332
Coder2003Anujyoti <anujyotide@gmail.com>
2025-05-02 12:31:01 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,8 @@
 import React,{useEffect} from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx"
-import Site from "./main/Site.jsx";
-import Auction from "./play/Auction.jsx";
-import Teams from "./play/Teams.jsx";
-import Stats from "./play/Stats.jsx";
-import Fixtures from "./play/Fixtures.jsx";
-import Game from "./play/Game.jsx";
 const App = () => {
   return (
-    <Router>
-      <Routes>
-<Route path="/" element={<Site />} />
-<Route path="/navbar" element={<Navbar  />} />
-<Route path="/auction" element={<Auction  />} /> 
-<Route path="/teams" element={<Teams  />} /> 
-<Route path="/stats" element={<Stats  />} />
-<Route path="/fixtures" element={<Fixtures  />} />
-<Route path="/game" element={<Game   />} />
-      </Routes>
-    </Router>
+  <Navbar />
   );
 };
 
--- frontend/src/play/teamLogos.js
+++
@@ -1,2 +0,0 @@
-const teams=["GG","UY","UMU","TTS","TT","PPN","PP","BB","JPP","BW","DD","HS"];
-export default teams;
